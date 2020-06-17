263407bee68cbbf9e2073974dee14b8fff243bf1
jiaqianjing <jiaqianjing@gmail.com>
2020-06-17 19:38:35 +0800

    add Dockerfile.arena

---
+++ examples/mnist/build_image_arena.sh
@@ -0,0 +1,1 @@
+docker build -f Dockerfile.arena -t registry.cn-huhehaote.aliyuncs.com/lumo/pytorch-dist-mnist-arena:1.0 ./
---
+++ examples/mnist/test_image.sh
@@ -0,0 +1,1 @@
+docker run --name mnist -it registry.cn-huhehaote.aliyuncs.com/lumo/pytorch-dist-mnist-test:1.0 "--backend" "gloo"
